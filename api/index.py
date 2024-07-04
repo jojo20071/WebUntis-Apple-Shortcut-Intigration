02095eecd9ea83f038006f9b6ee23d5246a764b9
Johannes <johanneshaederle18092@gmail.com>
2024-07-05 00:18:42 +0200

    Added Untis and new endpoint

--- api/index.py
+++ api/index.py
@@ -6,6 +6,29 @@ import ast
 
 app = Flask(__name__)
 
+
+
+today = datetime.date.today()
+monday = today - datetime.timedelta(days=today.weekday())
+friday = monday + datetime.timedelta(days=4)
+
+s = webuntis.Session(
+    server='https://herakles.webuntis.com',
+    username='haede5044',
+    password='Jojo1809!',
+    school='Erich-Kästner-Gym',
+    useragent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Safari/537.36'
+)
+
+s.login()
+
+
+#------------------------------------------------------------
+@app.route('/timetable', methods=['GET'])
+def tt():
+    return str(s.my_timetable(start=(today),end=(today)))
+
+
 @app.route('/homepage', methods=['GET'])
 def home():
     return("Homepage :)")
