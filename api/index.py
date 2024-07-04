6a2f3ec225bb04c0319947c29cebe1c4014271e3
Johannes <johanneshaederle18092@gmail.com>
2024-07-04 23:45:11 +0200

    added info and fixed spelling

--- api/index.py
+++ api/index.py
@@ -6,9 +6,13 @@ import ast
 
 app = Flask(__name__)
 
-@app.route('/homeoage', methods=['GET'])
+@app.route('/homepage', methods=['GET'])
 def home():
     return("Homepage :)")
+
+@app.route('/info', methods=['GET'])
+def info():
+    return("WebUntis-Apple-Shortcut-Intigration Api endpoint to intigrate your WebUntis timetable etc. to your Apple Shortcuts for automations")
 
 
 
